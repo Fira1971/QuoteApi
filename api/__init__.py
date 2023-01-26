a8b06a045b89353ba92e8809cfe18e958c728b50
Freeman_LW <firsov1971@yandex.ru>
2023-01-26 10:16:39 +0300

    Change api/__init__.py(return user) handler/token.py(remove g.user=user)

--- api/__init__.py
+++ api/__init__.py
@@ -23,13 +23,12 @@ def verify_password(username, password):
     user = UserModel.query.filter_by(username=username).first()
     if not user or not user.verify_password(password):
         return False
-    g.user = user
-    return True
+    # g.user = user
+    return user
 
 
 @token_auth.verify_token
 def verify_token(token):
     from api.models.user import UserModel
     user = UserModel.verify_auth_token(token)
-    print(f"{user=}")
     return user
